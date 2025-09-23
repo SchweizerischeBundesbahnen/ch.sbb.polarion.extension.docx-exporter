c68eab3757bd0beb361532c0f970e88ac3b85d71
Alex Nikonov <alexander.nikonoff@sbb.ch>
2025-09-23 13:22:33 +0200

    fix: comment rendering causes a NullPointerException (#90)
    Refs: #89

--- src/test/java/ch/sbb/polarion/extension/docx_exporter/util/WorkItemCommentsProcessorTest.java
+++ src/test/java/ch/sbb/polarion/extension/docx_exporter/util/WorkItemCommentsProcessorTest.java
@@ -22,36 +22,53 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
+@SuppressWarnings({"rawtypes", "unchecked"})
 public class WorkItemCommentsProcessorTest {
     @Test
     void addWorkItemComments_withOneWorkItemAndOneCommentTest() {
         // Arrange
         ProxyDocument mockDocument = mock(ProxyDocument.class);
         IWorkItem mockWorkItem = mock(IWorkItem.class);
-        IComment mockComment = mock(IComment.class);
+
         Text mockText = mock(Text.class);
         IProject mockProject = mock(IProject.class);
-        IUser mockUser = mock(IUser.class);
+
         IModule mockOldApi = mock(IModule.class);
 
         when(mockDocument.getOldApi()).thenReturn(mockOldApi);
         when(mockOldApi.getAllWorkItems()).thenReturn(List.of(mockWorkItem));
         when(mockWorkItem.getId()).thenReturn("EL-219");
-        IPObjectList mockObjectList = mock(IPObjectList.class);
-        when(mockObjectList.iterator()).thenReturn(List.of(mockComment).iterator());
-        when(mockWorkItem.getRootComments(false)).thenReturn(mockObjectList);
 
-        when(mockComment.getId()).thenReturn("c1");
-        when(mockComment.getTitle()).thenReturn("Title 1");
-        when(mockComment.getText()).thenReturn(mockText);
+        IUser mockUser1 = mock(IUser.class);
+        IComment mockComment1 = mock(IComment.class);
+        when(mockComment1.getId()).thenReturn("c1");
+        when(mockComment1.getTitle()).thenReturn("Title 1");
+        when(mockComment1.getText()).thenReturn(mockText);
         when(mockText.getContent()).thenReturn("This is a comment");
-        when(mockComment.getProject()).thenReturn(mockProject);
+        when(mockComment1.getProject()).thenReturn(mockProject);
         when(mockProject.getId()).thenReturn("Project1");
-        when(mockComment.getAuthor()).thenReturn(mockUser);
-        when(mockUser.getName()).thenReturn("System Administrator");
-        when(mockComment.getCreated()).thenReturn(new Date(0));
-        when(mockComment.isResolved()).thenReturn(false);
-        when(mockComment.getChildComments()).thenReturn(IPObjectList.EMPTY_POBJECTLIST);
+        when(mockComment1.getAuthor()).thenReturn(mockUser1);
+        when(mockUser1.getName()).thenReturn("System Administrator");
+        when(mockComment1.getCreated()).thenReturn(new Date(0));
+        when(mockComment1.isResolved()).thenReturn(false);
+        when(mockComment1.getChildComments()).thenReturn(IPObjectList.EMPTY_POBJECTLIST);
+
+        IUser mockUser2 = mock(IUser.class);
+        IComment mockComment2 = mock(IComment.class);
+        when(mockComment2.getId()).thenReturn("c2");
+        when(mockComment2.getTitle()).thenReturn("Title 2 visible");
+        when(mockComment2.getText()).thenReturn(null); // this time - no text
+        when(mockComment2.getProject()).thenReturn(mockProject);
+        when(mockProject.getId()).thenReturn("Project1");
+        when(mockComment2.getAuthor()).thenReturn(mockUser2);
+        when(mockUser2.getName()).thenReturn("Basic User");
+        when(mockComment2.getCreated()).thenReturn(new Date(0));
+        when(mockComment2.isResolved()).thenReturn(false);
+        when(mockComment2.getChildComments()).thenReturn(IPObjectList.EMPTY_POBJECTLIST);
+
+        IPObjectList mockObjectList = mock(IPObjectList.class);
+        when(mockObjectList.iterator()).thenReturn(List.of(mockComment1, mockComment2).iterator());
+        when(mockWorkItem.getRootComments(false)).thenReturn(mockObjectList);
 
         String inputHtml = "<div id=\"params=id=EL-219\">original content</div>";
 
@@ -66,5 +83,7 @@ public class WorkItemCommentsProcessorTest {
         assertThat(updatedHtml).contains("System Administrator");
         assertThat(updatedHtml).contains("level-0");
         assertThat(updatedHtml).contains("original content");
+        assertThat(updatedHtml).doesNotContain("Title 1");
+        assertThat(updatedHtml).contains("Title 2 visible");
     }
 }
