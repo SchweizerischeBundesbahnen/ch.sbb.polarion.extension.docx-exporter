3a4544352b3d9af135b17b2ecb7b46393328d3ec
e551763 <alexander.nikonoff@sbb.ch>
2025-11-19 13:02:10 +0100

    chore: extra/unnecessary css removal

--- src/main/java/ch/sbb/polarion/extension/docx_exporter/converter/HtmlToDocxConverter.java
+++ src/main/java/ch/sbb/polarion/extension/docx_exporter/converter/HtmlToDocxConverter.java
@@ -1,6 +1,5 @@
 package ch.sbb.polarion.extension.docx_exporter.converter;
 
-import ch.sbb.polarion.extension.docx_exporter.constants.HtmlTag;
 import ch.sbb.polarion.extension.docx_exporter.pandoc.service.PandocServiceConnector;
 import ch.sbb.polarion.extension.docx_exporter.pandoc.service.model.PandocParams;
 import ch.sbb.polarion.extension.generic.regex.RegexMatcher;
@@ -9,32 +8,27 @@ import ch.sbb.polarion.extension.docx_exporter.settings.LocalizationSettings;
 import ch.sbb.polarion.extension.docx_exporter.util.HtmlLogger;
 import ch.sbb.polarion.extension.docx_exporter.util.HtmlProcessor;
 import ch.sbb.polarion.extension.docx_exporter.util.DocxExporterFileResourceProvider;
-import ch.sbb.polarion.extension.docx_exporter.util.DocxTemplateProcessor;
 import ch.sbb.polarion.extension.docx_exporter.util.html.HtmlLinksHelper;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.jetbrains.annotations.VisibleForTesting;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 
 import java.util.List;
 
 public class HtmlToDocxConverter {
-    private final DocxTemplateProcessor docxTemplateProcessor;
     private final HtmlProcessor htmlProcessor;
     private final PandocServiceConnector pandocServiceConnector;
 
     public HtmlToDocxConverter() {
-        this.docxTemplateProcessor = new DocxTemplateProcessor();
         DocxExporterFileResourceProvider fileResourceProvider = new DocxExporterFileResourceProvider();
         this.htmlProcessor = new HtmlProcessor(fileResourceProvider, new LocalizationSettings(), new HtmlLinksHelper(fileResourceProvider));
         this.pandocServiceConnector = new PandocServiceConnector();
     }
 
     @VisibleForTesting
-    public HtmlToDocxConverter(DocxTemplateProcessor docxTemplateProcessor, HtmlProcessor htmlProcessor, PandocServiceConnector pandocServiceConnector) {
-        this.docxTemplateProcessor = docxTemplateProcessor;
+    public HtmlToDocxConverter(HtmlProcessor htmlProcessor, PandocServiceConnector pandocServiceConnector) {
         this.htmlProcessor = htmlProcessor;
         this.pandocServiceConnector = pandocServiceConnector;
     }
@@ -62,17 +56,6 @@ public class HtmlToDocxConverter {
     @VisibleForTesting
     String preprocessHtml(String origHtml) {
         Document document = Jsoup.parse(origHtml);
-        Element head = document.head();
-        @Nullable Element styleTag = head.selectFirst(HtmlTag.STYLE);
-
-        String additionalCss = docxTemplateProcessor.buildSizeCss();
-        if (additionalCss != null) {
-            if (styleTag != null) {
-                styleTag.appendText(additionalCss);
-            } else {
-                head.appendElement(HtmlTag.STYLE).text(additionalCss);
-            }
-        }
 
         htmlProcessor.replaceLinks(document);
 
