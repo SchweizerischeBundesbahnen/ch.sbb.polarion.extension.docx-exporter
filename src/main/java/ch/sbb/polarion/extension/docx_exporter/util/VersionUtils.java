1d4f2e7b1a31a61fd4a2bea09404b2814a9fd0d4
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2025-11-21 08:53:47 +0100

    chore: pandoc service 1.5 (#134)

--- src/main/java/ch/sbb/polarion/extension/docx_exporter/util/VersionUtils.java
+++ src/main/java/ch/sbb/polarion/extension/docx_exporter/util/VersionUtils.java
@@ -1,28 +1,14 @@
 package ch.sbb.polarion.extension.docx_exporter.util;
 
-import ch.sbb.polarion.extension.docx_exporter.util.configuration.PandocStatusProvider;
+import ch.sbb.polarion.extension.docx_exporter.util.exporter.Constants;
 import lombok.experimental.UtilityClass;
 import org.jetbrains.annotations.Nullable;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
 
 @UtilityClass
 public class VersionUtils {
 
     public static @Nullable String getLatestCompatibleVersionPandocService() {
-        try (InputStream input = PandocStatusProvider.class.getClassLoader().getResourceAsStream("versions.properties")) {
-            if (input == null) {
-                return null;
-            }
-
-            Properties properties = new Properties();
-            properties.load(input);
-            return properties.getProperty("pandoc-service.version");
-        } catch (IOException e) {
-            return null;
-        }
+        return ch.sbb.polarion.extension.generic.util.VersionUtils.getValueFromProperties(Constants.VERSION_FILE, "pandoc-service.version");
     }
 
 }
